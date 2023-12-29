9851d6fac1b6c8ae9a7fa0b7edeb730130d07780
Lars-Espen Kolberg <larsespen.kolberg@gmail.com>
2023-12-29 20:12:56 +0100

    har lagt til Quill

--- main.js
+++ main.js
@@ -1,35 +1,65 @@
-import * as monaco from 'monaco-editor';
+ import Quill from 'quill/dist/quill.js'
 
-self.MonacoEnvironment = {
-	getWorker: function (workerId, label) {
-		const getWorkerModule = (moduleUrl, label) => {
-			return new Worker(self.MonacoEnvironment.getWorkerUrl(moduleUrl), {
-				name: label,
-				type: 'module'
+
+ var toolbarOptions = [
+	['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+	[{ 'list': 'ordered'}, { 'list': 'bullet' }],
+	[{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+
+  
+	[{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+	[{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+	[{ 'font': [] }],
+	[{ 'align': [] }],
+	['clean']                                         // remove formatting button
+  ];
+
+var element; 
+var editor;
+
+ function init() {
+	
+	var editBtn = document.getElementById("edit");
+
+	
+
+	editBtn.addEventListener("click", () => {
+		editor = new Quill('#editor', {
+			modules: { toolbar: toolbarOptions },
+			theme: 'snow',
 			});
-		};
+			editor.enable(true);
+		 });
+	var savetBtn = document.getElementById("save");
+			
+		savetBtn.addEventListener("click", () => {
+			
+			editor.enable(false);
+			// call with instances of quill and your element
+			element = document.getElementById("editor")
+			
+			destroy_quill(editor, element);
+			
+	});
+   }
+   window.onload = init;
 
-		switch (label) {
-			case 'json':
-				return getWorkerModule('/monaco-editor/esm/vs/language/json/json.worker?worker', label);
-			case 'css':
-			case 'scss':
-			case 'less':
-				return getWorkerModule('/monaco-editor/esm/vs/language/css/css.worker?worker', label);
-			case 'html':
-			case 'handlebars':
-			case 'razor':
-				return getWorkerModule('/monaco-editor/esm/vs/language/html/html.worker?worker', label);
-			case 'typescript':
-			case 'javascript':
-				return getWorkerModule('/monaco-editor/esm/vs/language/typescript/ts.worker?worker', label);
-			default:
-				return getWorkerModule('/monaco-editor/esm/vs/editor/editor.worker?worker', label);
-		}
-	}
-};
+   function destroy_quill(quill, el) {
 
-monaco.editor.create(document.getElementById('container'), {
-	value: "function hello() {\n\talert('Hello world!');\n}",
-	language: 'javascript'
-});
+    // remove toolbox
+    quill.theme.modules.toolbar.container.remove();
+    // remove clipboard
+    quill.theme.modules.clipboard.container.remove();
+    // remove tooltip
+    quill.theme.tooltip.root.remove();
+	requestAnimationFrame(() => {
+		el.classList.remove("ql-container")
+	  });
+	
+	  // set inner html
+	  //el.innerHTML = quill.root.innerHTML
+
+
+}
+
+
